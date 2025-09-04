7cdd53d8066d5bfca45b4b9c4656ef9016e34b33
vegetason <irakozepaulin12@example.com>
2025-09-04 17:06:36 +0200

    Creating the UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback on your resumes!" },
@@ -9,7 +12,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track your resume & Resume Ratings</h1>
+        <h2>Review your submissions & check AI-powered feedback</h2>
+      </div>
 
+      {
+        resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => {
+              return <ResumeCard key={resume.id} resume={resume} />
+            })}
+          </div>
+        )
+      }
+    </section>
   </main>
 }
